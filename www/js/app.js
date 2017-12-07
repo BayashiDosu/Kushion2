3eb3d296e9e7e9ac95a5bb7017fca9121a586389
BayashiDosu <muraba84yashi@gmail.com>
2017-12-07 10:05:55 +0900

    Project's Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,9 @@
+// This is a JavaScript file
+
+    $(function(){
+      // Initialization code
+      $('ons-tabbar-item').on('click', function(e) {
+        ons.notification.alert('Button is tapped!');
+      })
+    });
+
